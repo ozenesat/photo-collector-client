8a5f86369e11ce43ebf93a9616d0547ee3110069
ozenesat <ozenesat@gmail.com>
2020-05-11 23:18:58 -0400

    Authorization
    User authorization part's reorganized and finalized.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,10 @@ import { Route } from 'react-router-dom'
 import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
 import Header from '../Header/Header'
-import SignUp from '../SignUp/SignUp'
-import SignIn from '../SignIn/SignIn'
-import SignOut from '../SignOut/SignOut'
-import ChangePassword from '../ChangePassword/ChangePassword'
+import SignUp from '../Auth/SignUp'
+import SignIn from '../Auth/SignIn'
+import SignOut from '../Auth/SignOut'
+import ChangePassword from '../Auth/ChangePassword'
 
 class App extends Component {
   constructor () {
