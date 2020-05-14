3c86654085d1daaf28bbefd8691540e1219e1d15
ozenesat <ozenesat@gmail.com>
2020-05-14 19:35:08 -0400

    Home Page Carousel
    Carusel is created for main home page. But still it requires styling.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,74 +1,75 @@
-import React from 'react'
-// import Carousel from 'react-bootstrap/Carousel'
-// import axios from 'axios'
-// import apiUrl from '../../apiConfig'
+import React, { useState } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
 
 const Home = () => {
-//  const [photos, setPhotos] = useState(null)
-  //  const [check, setCheck] = useState(true)
-  // axios({
-  //   url: `${apiUrl}/random-home`,
-  //   method: 'GET'
-  // })
-  // .then(res => {
-  //   setPhotos(res.data)
-  //   console.log(res.data)
-  //   // setCheck(false)
-  // })
-  // .catch(console.error)
+  const [photos, setPhotos] = useState(null)
+  const [contoroller, setController] = useState(false)
+  if (!contoroller) {
+    axios({
+      url: `${apiUrl}/random-home`,
+      method: 'GET'
+    })
+      .then(res => {
+        setPhotos(res.data.photos)
+        console.log(res.data.photos[0], 'photos - 0')
+        setController(true)
+      })
+      .catch(console.error)
+  }
 
-  //  console.log(photos, 'P')
-  // const photosJsx = (
-  //   <Carousel>
-  //     <Carousel.Item>
-  //       <img
-  //         className="d-block w-100"
-  //         src="holder.js/800x400?text=First slide&bg=373940"
-  //         alt="First slide"
-  //       />
-  //       <Carousel.Caption>
-  //         <h3>First slide label</h3>
-  //         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-  //       </Carousel.Caption>
-  //     </Carousel.Item>
-  //     <Carousel.Item>
-  //       <img
-  //         className="d-block w-100"
-  //         src="holder.js/800x400?text=Second slide&bg=282c34"
-  //         alt="Third slide"
-  //       />
-  //       {console.log('hi')}
-  //       <Carousel.Caption>
-  //         <h3>Second slide label</h3>
-  //         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  //       </Carousel.Caption>
-  //     </Carousel.Item>
-  //     <Carousel.Item>
-  //       <img
-  //         className="d-block w-100"
-  //         src="holder.js/800x400?text=Third slide&bg=20232a"
-  //         alt="Third slide"
-  //       />
-  //
-  //       <Carousel.Caption>
-  //         <h3>Third slide label</h3>
-  //         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-  //       </Carousel.Caption>
-  //     </Carousel.Item>
-  //   </Carousel>
-  // )
-  // if (!photos) {
-  //   return (
-  //     <div>
-  //       <h1> Welcome to Photo Collector</h1>
-  //       <p>Loading...</p>
-  //       {console.log(photos, 'PPP')}
-  //     </div>
-  //   )
-  // }
+  console.log(photos, 'P')
+  if (contoroller) {
+    const photosJsx = (
+      <Carousel style={{ display: 'flex', justifyContent: 'center', objectFit: 'contain' }}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`${photos[0].urls.regular}&w=1500&dpr=2`}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={photos[1].urls.raw + '&w=1500&dpr=2'}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`${photos[2].urls.regular}&w=1500&dpr=2`}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    )
+    return (
+      photosJsx
+    )
+  }
 
   return (
-    <h1>Welcome to Photos</h1>
+    <div>
+      <h1> Welcome to Photo Collector</h1>
+      <p>Loading...</p>
+      {console.log(photos, 'PPP')}
+    </div>
   )
 }
 
